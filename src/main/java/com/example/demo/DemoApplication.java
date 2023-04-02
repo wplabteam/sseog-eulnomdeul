34a630ea8cf263ec011889e89ac45145edea3c0b
hiimlyh <mylovedarts@naver.com>
2023-04-03 08:44:32 +0900

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.engine.IterationStatusVar;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,6 +10,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("하이 썩을");
+		System.out.println("test");
+
 	}
 
 }
